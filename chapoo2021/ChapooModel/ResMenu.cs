efaab0a7ebb24426f3f6306db4a9fb6bef78a35a
Farukb13 <34651324+Farukb13@users.noreply.github.com>
2021-05-10 17:15:12 +0200

    10 mei 2021
    - Tafels aangemaakt samen met code (buttons)
    - Menukaarten tonen met middel van knop

---
+++ chapoo2021/ChapooModel/ResMenu.cs
@@ -0,0 +1,17 @@
+﻿namespace ChapooModel
+{
+    // Made by Faruk Bikmaz
+    public class ResMenu
+    {
+        public int menuItemId;
+        public int menuId;
+        public decimal price;
+        public int stock;
+        public bool alcoholl;
+        public string itemName;
+
+    }
+    public class Lunch { public string itemName; public decimal price; }
+    public class Diner { public string itemName; public decimal price; }
+    public class Drankjes { public string itemName; public decimal price; public bool alcohol; }
+}
