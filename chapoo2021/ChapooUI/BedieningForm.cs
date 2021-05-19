86bbe28bb0969ba05c3efaecfbec80849a24a789
Farukb13 <34651324+Farukb13@users.noreply.github.com>
2021-05-19 15:42:51 +0200

    Tafel overzciht
    Elk tafel heeft nu zijn eigen buttons

--- chapoo2021/ChapooUI/BedieningForm.cs
+++ chapoo2021/ChapooUI/BedieningForm.cs
@@ -26,174 +26,133 @@ namespace ChapooUI
         {
             InitializeComponent();
             HidePanelsRecursively(this);
-            Table_pnl.Show();
+           // Table_pnl.Show();
         }
 
-        private void OrderBtn_Click(object sender, EventArgs e)             // Made by Faruk Bikmaz
-        {
-            // Made by Faruk Bikmaz
-            HidePanelsRecursively(this);
-            bestelling_pnl.Show();
+        /*        private void OrderBtn_Click(object sender, EventArgs e)             // Made by Faruk Bikmaz
+                {
+                    // Made by Faruk Bikmaz
+                    HidePanelsRecursively(this);
+                    bestelling_pnl.Show();
 
-            ChapooLogic.Tafels_Service tafel_service = new ChapooLogic.Tafels_Service();
-            List<Tafels> tafelList = tafel_service.GetAllFreeTables();
+                    ChapooLogic.Tafels_Service tafel_service = new ChapooLogic.Tafels_Service();
+                    List<Tafels> tafelList = tafel_service.GetAllFreeTables();
 
-            // clear the listview before filling it again
-            tafels_lv.Items.Clear();
+                    // clear the listview before filling it again
+                    tafels_lv.Items.Clear();
 
-            foreach (ChapooModel.Tafels t in tafelList)
-            {
-                string[] tafelArray = new string[] {
-                        t.tableId.ToString()
+                    foreach (ChapooModel.Tafels t in tafelList)
+                    {
+                        string[] tafelArray = new string[] {
+                                t.tableId.ToString()
+                            };
+
+                        ListViewItem item = new ListViewItem(tafelArray);
+
+                        tafels_lv.Items.Add(item);
+                    }
+                }
+
+                private void closeBtn_Click(object sender, EventArgs e)             // Made by Faruk Bikmaz
+                {
+                    HidePanelsRecursively(this);
+                    Table_pnl.Show();
+                }
+
+                private void continueBtn_Click(object sender, EventArgs e)             // Made by Faruk Bikmaz
+                {
+                    if (tafels_lv.SelectedItems.Count > 0)
+                    {
+                        HidePanelsRecursively(this);
+                        bestelling2_pnl.Show();
+                    } else
+                    {
+                        MessageBox.Show("Selecteer een tafel");
+                    }
+                }
+
+                private void close2Btn_Click(object sender, EventArgs e)             // Made by Faruk Bikmaz
+                {
+                    HidePanelsRecursively(this);
+                    Table_pnl.Show();
+                }
+
+                private void lunchBtn_Click(object sender, EventArgs e)             // Made by Faruk Bikmaz
+                {
+                    menu_lv.Show();
+                    menu_lv.Items.Clear();
+                    ChapooLogic.Menu_Service menu_service = new ChapooLogic.Menu_Service();
+                    List<Lunch> lunchList = menu_service.GetLunch();
+
+                    foreach (ChapooModel.Lunch l in lunchList)
+                    {
+                        string[] menuArray = new string[] {
+                                l.itemName,
+                                l.price.ToString("F")
                     };
 
-                ListViewItem item = new ListViewItem(tafelArray);
+                    ListViewItem item = new ListViewItem(menuArray);
+                    menu_lv.Items.Add(item);
+                    }
+                }
 
-                tafels_lv.Items.Add(item);
-            }
-        }
+                private void dinerBtn_Click(object sender, EventArgs e)             // Made by Faruk Bikmaz
+                {
+                    menu_lv.Show();
+                    menu_lv.Items.Clear();
 
-        private void closeBtn_Click(object sender, EventArgs e)             // Made by Faruk Bikmaz
-        {
-            HidePanelsRecursively(this);
-            Table_pnl.Show();
-        }
+                    ChapooLogic.Menu_Service menu_service = new ChapooLogic.Menu_Service();
+                    List<Diner> dinerList = menu_service.GetDiner();
 
-        private void continueBtn_Click(object sender, EventArgs e)             // Made by Faruk Bikmaz
-        {
-            if (tafels_lv.SelectedItems.Count > 0)
-            {
-                HidePanelsRecursively(this);
-                bestelling2_pnl.Show();
-            } else
-            {
-                MessageBox.Show("Selecteer een tafel");
-            }
-        }
+                    foreach (ChapooModel.Diner d in dinerList)
+                    {
+                        string[] menuArray = new string[] {
+                                d.itemName,
+                                d.price.ToString("F")
+                    };
 
-        private void close2Btn_Click(object sender, EventArgs e)             // Made by Faruk Bikmaz
-        {
-            HidePanelsRecursively(this);
-            Table_pnl.Show();
-        }
+                    ListViewItem item = new ListViewItem(menuArray);
+                    menu_lv.Items.Add(item);
+                    }
+                }
 
-        private void lunchBtn_Click(object sender, EventArgs e)             // Made by Faruk Bikmaz
-        {
-            menu_lv.Show();
-            menu_lv.Items.Clear();
-            ChapooLogic.Menu_Service menu_service = new ChapooLogic.Menu_Service();
-            List<Lunch> lunchList = menu_service.GetLunch();
+                private void drankBtn_Click(object sender, EventArgs e)              // Made by Faruk Bikmaz
+                {
+                    menu_lv.Show();
+                    menu_lv.Items.Clear();
 
-            foreach (ChapooModel.Lunch l in lunchList)
-            {
-                string[] menuArray = new string[] {
-                        l.itemName,
-                        l.price.ToString("F")
-            };
+                    ChapooLogic.Menu_Service menu_service = new ChapooLogic.Menu_Service();
+                    List<Drankjes> drankList = menu_service.GetDrankjes();
 
-            ListViewItem item = new ListViewItem(menuArray);
-            menu_lv.Items.Add(item);
-            }
-        }
+                    foreach (ChapooModel.Drankjes dra in drankList)
+                    {
+                        string[] menuArray = new string[] {
+                                dra.itemName,
+                                dra.price.ToString("F"),
+                                dra.alcohol.ToString()     
+                    };
 
-        private void dinerBtn_Click(object sender, EventArgs e)             // Made by Faruk Bikmaz
-        {
-            menu_lv.Show();
-            menu_lv.Items.Clear();
+                    ListViewItem item = new ListViewItem(menuArray);
+                    menu_lv.Items.Add(item);
+                    }
+                }
 
-            ChapooLogic.Menu_Service menu_service = new ChapooLogic.Menu_Service();
-            List<Diner> dinerList = menu_service.GetDiner();
-
-            foreach (ChapooModel.Diner d in dinerList)
-            {
-                string[] menuArray = new string[] {
-                        d.itemName,
-                        d.price.ToString("F")
-            };
-
-            ListViewItem item = new ListViewItem(menuArray);
-            menu_lv.Items.Add(item);
-            }
-        }
-
-        private void drankBtn_Click(object sender, EventArgs e)              // Made by Faruk Bikmaz
-        {
-            menu_lv.Show();
-            menu_lv.Items.Clear();
-
-            ChapooLogic.Menu_Service menu_service = new ChapooLogic.Menu_Service();
-            List<Drankjes> drankList = menu_service.GetDrankjes();
-
-            foreach (ChapooModel.Drankjes dra in drankList)
-            {
-                string[] menuArray = new string[] {
-                        dra.itemName,
-                        dra.price.ToString("F"),
-                        dra.alcohol.ToString()     
-            };
-            
-            ListViewItem item = new ListViewItem(menuArray);
-            menu_lv.Items.Add(item);
-            }
-        }
-
-        private void amount_TextChanged(object sender, EventArgs e)              // Made by Faruk Bikmaz
-        {
-            if (System.Text.RegularExpressions.Regex.IsMatch(amount.Text, "[^0-9]"))
-            {
-                MessageBox.Show("Alleen cijfers mogelijk");
-                amount.Text = amount.Text.Remove(amount.Text.Length - 1);
-            }
-        }
+                private void amount_TextChanged(object sender, EventArgs e)              // Made by Faruk Bikmaz
+                {
+                    if (System.Text.RegularExpressions.Regex.IsMatch(amount.Text, "[^0-9]"))
+                    {
+                        MessageBox.Show("Alleen cijfers mogelijk");
+                        amount.Text = amount.Text.Remove(amount.Text.Length - 1);
+                    }
+                }*/
         // TAFELS
-        private void Tafel1Btn_Click(object sender, EventArgs e)
+        private void Table1_pnl_Paint(object sender, PaintEventArgs e)
         {
-
-        }
-
-        private void Tafel2Btn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Tafel3Btn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Tafel4Btn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Tafel5Btn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Tafel6Btn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Tafel7Btn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Tafel8Btn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Tafel9Btn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Tafel10Btn_Click(object sender, EventArgs e)
-        {
-
+            System.Windows.Forms.Panel panel = sender as System.Windows.Forms.Panel;
+            System.Drawing.Rectangle rect = panel.ClientRectangle;
+            rect.Width--;
+            rect.Height--;
+            e.Graphics.DrawRectangle(Pens.Red, rect);
         }
     }
 }
